542843c4fa36292b0faa4ef138ae1c0dec68002a
Ameen <your-email@example.com>
2026-06-25 14:43:43 +0530

    ameen

--- python-1/app.py
+++ python-1/app.py
@@ -1,1 +1,14 @@
-print("ameen mamu.....") 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Flask Application"
+
+@app.route("/health")
+def health():
+    return "Application is Healthy"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
